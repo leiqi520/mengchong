a7bf5065a4eb51dbabc87f76bc86d345ce5a9a76
雷磊14755338929 <1951668982@qq.com>
2023-12-25 21:45:33 +0800

    登录加二维码

---
+++ src/components/QRindexView.vue
@@ -0,0 +1,54 @@
+<template>
+  <canvas id="QRCode_header" style="width: 190px; height: 190px"></canvas>
+</template>
+
+<script>
+import QRCode from 'qrcode' //引入生成二维码插件
+export default {
+  props: [''],
+  data() {
+    return {
+      qrUrl: ''
+    }
+  },
+  watch: {},
+  mounted() {
+    this.getQRCode()
+  },
+  created() {},
+  methods: {
+    getQRCode() {
+      //生成的二维码为URL地址js
+      this.qrUrl = 'https://152.136.245.230:7784/play/index.html?uid=1&zid=1&roomid=53'
+      let opts = {
+        errorCorrectionLevel: 'H', //容错级别
+        type: 'image/png', //生成的二维码类型
+        quality: 0.3, //二维码质量
+        margin: 0, //二维码留白边距
+        width: 190, //宽
+        height: 190, //高
+        text: 'http://www.xxx.com', //二维码内容
+        color: {
+          dark: '#333333', //前景色
+          light: '#fff' //背景色
+        }
+      }
+
+      let msg = document.getElementById('QRCode_header')
+      // 将获取到的数据（val）画到msg（canvas）上
+      QRCode.toCanvas(msg, this.qrUrl, opts, function (error) {
+        if (error) {
+          console.log('二维码加载失败', error)
+          this.$message.error('二维码加载失败')
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+canvas {
+  margin-left: 95px;
+}
+</style>
--- src/view/ExploreView.vue
+++ src/view/ExploreView.vue
@@ -21,7 +21,7 @@ import type { isJsxOpeningElement } from 'typescript';
 
           <el-col :span="4">
             <div class="grid-content ep-bg-purple" />
-            <button class="chuangzuo">创作中心</button>
+            <button class="chuangzuo" @click="dialogTableVisible = true">创作中心</button>
           </el-col>
         </el-row>
       </el-header>
@@ -46,13 +46,13 @@ import type { isJsxOpeningElement } from 'typescript';
               <el-icon><DocumentAdd /></el-icon>
               <span>发布</span>
             </el-menu-item>
-            <el-menu-item index="3">
+            <el-menu-item index="3" @click="dialogTableVisible = true">
               <el-icon><Bell /></el-icon>
               <span>通知</span>
             </el-menu-item>
           </el-menu>
-          <el-button type="danger" class="login">登录</el-button>
-          <el-card class="box-card">
+          <el-button type="danger" class="login" @click="dialogTableVisible = true">登录</el-button>
+          <el-card class="box-card" @click="dialogTableVisible = true">
             <h5>马上登录即可</h5>
             <p>刷到更懂你的优质内容</p>
           </el-card>
@@ -61,28 +61,46 @@ import type { isJsxOpeningElement } from 'typescript';
         <!-- 右侧 -->
         <el-main>
           <el-scrollbar height="580px">
-            <el-row :gutter="20">
-              <el-col v-for="o in 8" :key="o" :span="6">
-                <el-card :body-style="{ padding: '0px' }">
-                  <img
-                    src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
-                    class="image"
-                  />
-                  <div style="padding: 14px">
-                    <span>Yummy hamburger</span>
-                    <div class="bottom">
-                      <time class="time">{{ currentDate }}</time>
-                      <el-button text class="button">Operating</el-button>
-                    </div>
-                  </div>
-                </el-card>
-              </el-col>
-            </el-row>
+            <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
+              <el-tab-pane label="推荐" name="first">
+                <el-row :gutter="20">
+                  <el-col v-for="o in 8" :key="o" :span="6">
+                    <el-card :body-style="{ padding: '0px' }">
+                      <img
+                        src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+                        class="image"
+                      />
+                      <div style="padding: 14px">
+                        <span>Yummy hamburger</span>
+                        <div class="bottom">
+                          <time class="time">{{ currentDate }}</time>
+                          <el-button text class="button">Operating</el-button>
+                        </div>
+                      </div>
+                    </el-card>
+                  </el-col>
+                </el-row>
+              </el-tab-pane>
+              <el-tab-pane label="猫" name="second">Config</el-tab-pane>
+              <el-tab-pane label="狗" name="third">Role</el-tab-pane>
+              <el-tab-pane label="其他" name="fourth">Task</el-tab-pane>
+            </el-tabs>
           </el-scrollbar>
         </el-main>
       </el-container>
     </el-container>
   </div>
+  <!-- 登录对话框 -->
+  <el-dialog v-model="dialogTableVisible" align-center width="54%">
+    <el-container>
+      <el-aside width="380px" class="diaLeft">
+        <p>宠萌</p>
+        <QRcode width="100" height="100"></QRcode>
+        <h4>使用QQ / 微信扫码登录</h4>
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-dialog>
 </template>
 
 <script setup lang="ts">
@@ -91,16 +109,24 @@ import { Search } from '@element-plus/icons-vue'
 const input3 = ref('')
 
 //左侧
-const isopen: boolean = ref(1)
+const isopen = ref(true)
 const handleSelect = (key: string, keyPath: string[]) => {
   console.log(key, keyPath)
-  if (key !== '1') {
-    isopen.value = 0
-  } else isopen.value = 1
+  if (key !== '1') isopen.value = false
+  else isopen.value = true
 }
-
 //右侧
 const currentDate = ref(new Date())
+
+import type { TabsPaneContext } from 'element-plus'
+const activeName = ref('first')
+const handleClick = (tab: TabsPaneContext, event: Event) => {
+  console.log(tab, event)
+}
+
+// 对话框
+const dialogTableVisible = ref(false)
+import QRcode from '@/components/QRindexView.vue'
 </script>
 
 <style scoped lang="less">
@@ -110,14 +136,12 @@ label {
   color: #fc2b2b;
   line-height: 60px;
 }
-
 .el-input {
   position: relative;
   top: 25%;
   border-radius: 30px;
   background-color: rgba(0, 0, 0, 0.03);
 }
-
 .chuangzuo {
   width: 100px;
   height: 30px;
@@ -134,23 +158,19 @@ label {
   background-color: #e4e0e0;
   border: 0;
 }
-
 .el-menu {
   border-right: 0;
   color: black;
   font-weight: 700;
 }
-
 .el-menu-item {
   height: 60px;
   border-radius: 30px;
   margin-bottom: 10px;
 }
-
 .el-menu-item:focus {
   background: rgba(0, 0, 0, 0.03);
 }
-
 .login {
   margin-bottom: 20px;
   width: 100%;
@@ -167,12 +187,10 @@ label {
     font-size: 14px;
   }
 }
-
 .time {
   font-size: 12px;
   color: #999;
 }
-
 .bottom {
   margin-top: 13px;
   line-height: 12px;
@@ -193,5 +211,52 @@ label {
 
 .el-col {
   margin-bottom: 20px;
+}
+
+/deep/ .el-tabs__nav-wrap {
+  &::after {
+    display: none;
+  }
+}
+/deep/ .el-tabs__item {
+  color: rgba(51, 51, 51, 0.8);
+}
+
+/deep/ .el-tabs__active-bar {
+  background-color: #fff;
+}
+
+/deep/ .el-tabs__item.is-active {
+  width: 85px;
+  color: #333333;
+  font-weight: bold;
+  font-size: 16px;
+  position: relative;
+  background: rgba(0, 0, 0, 0.03);
+  text-align: center;
+  border-radius: 20px;
+}
+/deep/ .el-tabs--top .el-tabs__item.is-top:nth-child(2) {
+  padding-left: 20px;
+}
+/deep/ .el-tabs--top .el-tabs__item.is-top:last-child {
+  padding-right: 20px;
+}
+// 对话框
+.diaLeft {
+  p {
+    font-size: 26px;
+    font-weight: 600;
+    color: #fc2b2b;
+    line-height: 60px;
+    text-align: center;
+  }
+  img {
+    width: 190px;
+    margin-left: 95px;
+  }
+  h4 {
+    text-align: center;
+  }
 }
 </style>
